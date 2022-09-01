a24778ae152fcec73079c6de5cbea1da83163ede
Kevin Noonan <kevin@yesenergy.com>
2022-09-01 13:02:20 -0400

    * Added StoredProcExecutor * postgres to dev mt container * Added a test to verify connectivity

---
+++ middletier/service/src/main/java/com/yesenergy/service/YesResultSetExtractor.java
@@ -0,0 +1,65 @@
+package com.yesenergy.service;
+
+import java.sql.Clob;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.ResultSetExtractor;
+
+public class YesResultSetExtractor implements ResultSetExtractor<ArrayList<HashMap<String, Object>>> {
+
+    @Override
+    public ArrayList<HashMap<String, Object>> extractData(ResultSet res) throws SQLException, DataAccessException {
+        ResultSetMetaData rsmd = res.getMetaData();
+		int numColumns = rsmd.getColumnCount();
+		String[] cols = new String[numColumns];
+		for (int i = 1; i < numColumns + 1; i++) {
+			String columnName = rsmd.getColumnName(i);
+			cols[i - 1] = columnName;
+		}
+			
+        ArrayList<HashMap<String, Object>> results = new ArrayList<HashMap<String, Object>>();
+		Integer rows = 0;
+		// SimpleDateFormat df = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
+
+		while (res.next()) {
+			HashMap<String, Object> row = new HashMap<String, Object>();
+			for (String col : cols) {
+				Object val = res.getObject(col);
+				if (val instanceof Timestamp) {
+                    Timestamp val2 = (Timestamp) val;
+                    Date d = new Date(val2.getTime());
+                    //row.put(col, df.format(d));
+                } else if (val instanceof Clob) {
+                    Clob clobVal = (Clob) val;
+    
+                    String strVal;
+    
+                    if (clobVal.length() < Integer.MAX_VALUE) {
+                        strVal = clobVal.getSubString(1, (int) clobVal.length());
+                        row.put(col, strVal);
+                    }
+                    // Free the CLOBs!, this helps with PGA memory
+                    if (clobVal != null) {
+                        clobVal.free();
+                    }
+    
+                } else {
+                    row.put(col, val);
+                }
+			}
+			results.add(row);
+			rows++;
+		}
+
+		return results;
+    }
+
+
+}
